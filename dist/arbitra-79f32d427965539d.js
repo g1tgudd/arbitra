c719fcc082e2493931ea254e73a3a46bb9ec73aa
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2023-02-11 01:20:34 +0700

    Commit

--- dist/arbitra-79f32d427965539d.js
+++ dist/arbitra-79f32d427965539d.js
@@ -579,7 +579,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('arbitra-8baa70757ba82302_bg.wasm', import.meta.url);
+        input = new URL('arbitra-79f32d427965539d_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
